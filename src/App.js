23cc768de3e8c330285bd70cdb837df4e62b5f84
Deepika <kulkdeepika@gmail.com>
2020-05-01 20:06:09 -0700

    changed router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Index from './pages/Index';
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -8,10 +8,10 @@ import Contact from "./pages/Contact"
 function App() {
   return (
     <Router>
-        <Route exact path= "/portfolioreact/" component={Index}></Route>
-        <Route exact path= "/portfolioreact/about" component={About}></Route>
-        <Route exact path= "/portfolioreact/portfolio" component={Portfolio}></Route>
-        <Route exact path= "/portfolioreact/Contact" component={Contact}></Route>
+        <Route exact path= "/" component={Index}></Route>
+        <Route exact path= "/about" component={About}></Route>
+        <Route exact path= "/portfolio" component={Portfolio}></Route>
+        <Route exact path= "/Contact" component={Contact}></Route>
     </Router>
   );
 }
